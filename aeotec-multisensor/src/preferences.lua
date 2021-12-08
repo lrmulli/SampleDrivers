c9153ac19257e65e1d3c2d3ef032fe2ba2598612
louis mullineux <lmullineux@gmail.com>
2021-12-08 08:31:04 +0000

    removed sample drivers

--- aeotec-multisensor/src/preferences.lua
+++
@@ -1,6 +0,0 @@
-local preferences = {
-  motionSensorSensitivity = {parameter_number = 4, size = 1},
-  motionClearTime = {parameter_number = 3, size = 2}
-}
-
-return preferences
--- hello-world/src/test.lua
+++
@@ -1,2 +0,0 @@
-local bit = require 'bitop.funcs'
-print(bit.bor(0xff00ff00, 0x00ff00ff))
