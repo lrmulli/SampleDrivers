974197da4c5f1671bb0a2821341f089ade5a87b2
lrmulli <lmullineux@gmail.com>
2022-08-25 14:23:57 +0100

    swapped lines around

--- harmony-bridge-simple/src/init.lua
+++ harmony-bridge-simple/src/init.lua
@@ -88,8 +88,8 @@ end
 -- this is called when a device is removed by the cloud and synchronized down to the hub
 local function device_removed(driver, device)
   local ws = device:get_field("ws")
-  ws:close(4001,'lost interest')
   hello_world_driver:unregister_channel_handler(ws.sock)
+  ws:close(4001,'lost interest')
   log.info("[" .. device.id .. "] Removing Harmony device")
 end
 
