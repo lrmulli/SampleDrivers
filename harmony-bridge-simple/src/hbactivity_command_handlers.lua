cdd34dba82bf5923b2708a891a1f2a27c88807e1
lrmulli <lmullineux@gmail.com>
2022-09-06 12:40:10 +0100

    added on command to activities

--- harmony-bridge-simple/src/hbactivity_command_handlers.lua
+++ harmony-bridge-simple/src/hbactivity_command_handlers.lua
@@ -6,7 +6,10 @@ local hbactivity_command_handlers = {}
 -- callback to handle an `on` capability command
 function hbactivity_command_handlers.switch_on(driver, device, command)
   log.debug(string.format("[%s] calling set_power(on)", device.device_network_id))
-  device:emit_event(capabilities.switch.switch.on())
+  --device:emit_event(capabilities.switch.switch.on())
+  parent = hbactivity_command_handlers.getParentDevice(driver,device)
+
+  sendHarmonyStartActivity(parent,"-1",0)
 end
 
 -- callback to handle an `off` capability command
@@ -14,7 +17,8 @@ function hbactivity_command_handlers.switch_off(driver, device, command)
   log.debug(string.format("[%s] calling set_power(off)", device.device_network_id))
   --device:emit_event(capabilities.switch.switch.off())
   parent = hbactivity_command_handlers.getParentDevice(driver,device)
-  sendHarmonyStartActivity(parent,"-1",0)
+  activityid = device.vendor_provided_label
+  sendHarmonyStartActivity(parent,"activityid",0)
 end
 
 function hbactivity_command_handlers.getParentDevice(driver,device)
