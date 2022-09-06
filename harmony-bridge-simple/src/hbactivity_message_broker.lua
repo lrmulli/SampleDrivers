e33515ef053ead951f523452b415f7196fe58049
lrmulli <lmullineux@gmail.com>
2022-09-06 11:19:36 +0100

    f

--- harmony-bridge-simple/src/hbactivity_message_broker.lua
+++ harmony-bridge-simple/src/hbactivity_message_broker.lua
@@ -17,9 +17,15 @@ function hbactivity_message_broker.activityMessageReceived(driver,device,msg)
     log.info("Activity Message Broker - Received: ",utils.stringify_table(msg,"Activity Message: ",true))
     local device_list = driver:get_devices()
     for _, d in ipairs(device_list) do
-        log.info("DeviceID: ",d.id)
-        log.info("Device Parent: ",d.parent_device_id)
-        log.info("Device Activity:",d.vendor_provided_label)
+        if d.parent_device_id == device.id then
+            --this means that the child device is owned by the device that received the message
+            log.info("DeviceID: ",d.id)
+            log.info("Device Parent: ",d.parent_device_id)
+            log.info("Device Activity:",d.vendor_provided_label)
+            if msg.type == "connect.stateDigest?notify" then
+                d:emit_event(logger.logger("Activity Message Recd: "..(msg or "")))
+            end
+        end
     end
 end
 
