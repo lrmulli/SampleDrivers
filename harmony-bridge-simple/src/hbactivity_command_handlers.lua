7a34650f7fed7e7bc15b4b149676d764f37a92d2
lrmulli <lmullineux@gmail.com>
2022-09-06 12:30:10 +0100

    f

--- harmony-bridge-simple/src/hbactivity_command_handlers.lua
+++ harmony-bridge-simple/src/hbactivity_command_handlers.lua
@@ -12,7 +12,20 @@ end
 -- callback to handle an `off` capability command
 function hbactivity_command_handlers.switch_off(driver, device, command)
   log.debug(string.format("[%s] calling set_power(off)", device.device_network_id))
-  device:emit_event(capabilities.switch.switch.off())
+  --device:emit_event(capabilities.switch.switch.off())
+  parent = hbactivity_command_handlers.getParentDevice(device)
+  sendHarmonyStartActivity(parent,"-1",0)
 end
 
+function hbactivity_command_handlers.getParentDevice(device)
+    local device_list = driver:get_devices()
+    local dev = {}
+    for _, d in ipairs(device_list) do
+        if device.parent_device_id == d.id then
+            dev = d
+        end
+    end
+    log.info("Returning Parent: ",dev.id)
+    return dev
+end
 return hbactivity_command_handlers
