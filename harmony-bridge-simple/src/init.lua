879be64b242976c3c577e8f340e790d577872294
lrmulli <lmullineux@gmail.com>
2023-03-24 11:29:51 +0000

    fixed bug in get config

--- harmony-bridge-simple/src/init.lua
+++ harmony-bridge-simple/src/init.lua
@@ -225,7 +225,7 @@ function receiveConfig(device,config)
     for i,cg in pairs(d.controlGroup) do
       for x, action in pairs(cg["function"]) do
         --print(utils.stringify_table(action))
-        local cmd = action.action.name
+        local cmd = action.action.command
         print(cmd)
         deviceListString = deviceListString..[[{"deviceId":"]]..d.id..[[","command":"]]..action.name..[[","action":"press"}]]..string.char(10)..string.char(13)
       end
